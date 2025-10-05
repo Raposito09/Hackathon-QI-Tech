81634651dc953182dd4ba4aedce39d5d11d4ced1
Mariana Aquino Rocha <maquinorocha@usp.br>
2025-10-05 10:52:54 -0300

    Loan

--- backend-core-api/src/main/java/com/inovacamp/core_api/application/controller/LoanController.java
+++ backend-core-api/src/main/java/com/inovacamp/core_api/application/controller/LoanController.java
@@ -1,14 +1,14 @@
 package com.inovacamp.core_api.application.controller;
 
-import com.inovacamp.core_api.application.dto.LoanRequest;
-import com.inovacamp.core_api.application.dto.LoanResponse;
-import com.inovacamp.core_api.application.dto.MarketplaceResponse;
+import com.inovacamp.core_api.application.dto.*;
 import com.inovacamp.core_api.domain.service.LoanService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/loans")
@@ -30,5 +30,13 @@ public class LoanController {
         return loanService.getMarketplaceLoans();
     }
 
+    @PostMapping("/{loanId}/fund")
+    @SecurityRequirement(name = "bearerAuth")
+    public FundLoanResponse fundLoan(
+            @PathVariable UUID loanId,
+            @Valid @RequestBody FundLoanRequest request
+    ) {
+        return loanService.fundLoan(loanId, request);
+    }
 
 }
